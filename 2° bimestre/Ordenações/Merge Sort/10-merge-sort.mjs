86b75341979c2f522f5983f5ffc1efc505396c7a
Lucas Monteiro <lucasantoniomonteiro1@gmail.com>
2026-08-26 22:03:46 -0300

    adiciona finalização de merge sort

--- 2° bimestre/Ordenações/Merge Sort/10-merge-sort.mjs
+++ 2° bimestre/Ordenações/Merge Sort/10-merge-sort.mjs
@@ -14,14 +14,33 @@ function mergeSort(vetor) {
 
     let posEsq = 0;
     let posDir = 0;
-    vetRes = [];
-    while(posEsq < vetEsq.length && posDir < vetDir.length7) {
+    let vetRes = [];
+    while(posEsq < vetEsq.length && posDir < vetDir.length) {
         if(vetEsq[posEsq] < vetDir[posDir]) {
             vetRes.push(vetEsq[posEsq]);
             posEsq++;
+        } else {
+            vetRes.push(vetDir[posDir])
+            posDir++;
         }
     }
+
+    let sobra;
+    if(posEsq < posDir) {
+        sobra = vetEsq.slice(posEsq);
+    } else {
+        sobra = vetDir.slice(posDir);
+    }
+
+
+    return [...vetRes, ...sobra];
 }
 
 let num = [77,44,22,33,99,55,88,0,66,11]
 let num2 = [5,7,9,2,1,10,11]
+
+let numsOrdenado = mergeSort(num)
+console.log(numsOrdenado);
+
+let num2Ordenado = mergeSort(num2)
+console.log(num2Ordenado);
